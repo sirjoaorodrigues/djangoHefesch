aa4682856d0cbae34f1712e7b5dc147fb9a8450a
João Rodrigues <94868422+sirjoaorodrigues@users.noreply.github.com>
2022-07-16 14:30:23 -0300

    Update settings.py

--- djangoHefesch/settings.py
+++ djangoHefesch/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o=hdvofa35j)1gye+9%+-h_vw42ay%tr9q@b!bh0o_b#1v0#=t'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
